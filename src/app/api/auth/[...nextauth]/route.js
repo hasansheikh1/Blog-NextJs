b5df7b6d196435d9f1b70ae38067e8e56edbc35e
Ali Hassnain <ali1397hasnain@gmail.com>
2024-01-01 17:38:50 +0500

    changes

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,10 @@
 
 import NextAuth from "next-auth"
-
 import { authOptions } from "@/utils/auth"
 
-export default NextAuth(authOptions)
+const handler = NextAuth(authOptions)
+export { handler as GET, handler as POST }
+
+// export const POST = (req) => {
+
+// }
--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,29 +1,24 @@
 "use client"
-
 import React from 'react'
-import styles from"./loginPage.module.css"
+import styles from "./loginPage.module.css"
 import { signIn, useSession } from 'next-auth/react'
 function page() {
-  
 
 
-    const {data,status}=useSession();
+
+    const { data, status } = useSession();
     return (
 
-    <div className={styles.container}>
-        <div className={styles.wrapper}>
-            <div className={styles.socialButton} onClick={()=>
-              (
-                signIn("google")
-              )
-            } >Sign In with Google</div>
-            <div className={styles.socialButton}>Sign In with Github</div>
-            <div className={styles.socialButton}>Sign In with Facebook</div>
-            
-            </div>      
+        <div className={styles.container}>
+            <div className={styles.wrapper}>
+                <div className={styles.socialButton} onClick={() => signIn("google")} >Sign In with Google</div>
+                <div className={styles.socialButton}>Sign In with Github</div>
+                <div className={styles.socialButton}>Sign In with Facebook</div>
 
-    </div>
-  )
+            </div>
+
+        </div>
+    )
 }
 
 export default page
